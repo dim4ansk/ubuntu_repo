fec2fae781132ce7a059e7dd7a55082a1bc2d961
dima <dimablagoy02@gmail.com>
2025-03-21 14:27:45 +0000

    task from third homework

---
+++ HW3/task3.py
@@ -0,0 +1,18 @@
+# task 1
+print("I love Python"'\n' * 6)
+
+
+# task 2, 3, 4
+age_in_years = int(input("Enter your age: "))
+age_in_month = age_in_years * 12
+my_age = f'My name is {input("Enter your name: ")}. I`m {age_in_years} years old.'
+print(my_age)
+
+# task 5
+number = 1
+print(number > 0, number != 4, number < 1, number == 7, type(number) == type(int()),  sep=' ')
+
+# task 6
+a, b, c = 2, 5, 6
+d = str(a) + str(b) + str(c)
+print(d)
